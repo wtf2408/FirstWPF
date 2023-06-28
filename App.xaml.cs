20c8701921dc886c3cc7a7316df0a057672a6d67
wtf2408 <bogdanevseyv@yandex.ru>
2023-06-28 13:50:01 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace FirstWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //MessageBox.Show("hello world");
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[] words;
+        public MainWindow()
+        {
+            
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            words = inputText.Text.Split(' ');
+            foreach (string word in words)
+            {
+                listBox.Items.Add(word);
+            }
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            label.Content = ReversText(inputText.Text);
+        }
+        string ReversText(string text)
+        {
+            string[] tempText = text.Split(' ');
+            text = string.Empty;
+            foreach (string word in tempText)
+            {
+                text = $" {word}" + text; 
+            }
+            return text;
+        }
+    }
+}
